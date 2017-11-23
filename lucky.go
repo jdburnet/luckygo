2bbb46cd32eb153d2cd0ed85e813e578e81531c6
Jackson Burnette <jacksondburnette@gmail.com>
2017-11-23 11:34:20 -0800

    print max_results urls to stdout

--- lucky.go
+++ lucky.go
@@ -2,15 +2,54 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"golang.org/x/net/html"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	const google = "https://google.com"
+	const search = google + "/search?q="
+	const tmp = "funny+cat"
+	const url_prefix = "/url?q="
+	const max_results = 5
 
-	response, _ := http.Get(google)
-	rawhtml, _ := ioutil.ReadAll(response.Body)
+	response, _ := http.Get(search + tmp)
+	tokenizer := html.NewTokenizer(response.Body)
 
-	fmt.Printf("Raw HTML from %s\n\n\n%s", google, rawhtml)
+	results := 0
+
+	for {
+		tagtok := tokenizer.Next()
+
+		switch {
+
+		case tagtok == html.ErrorToken:
+			// End of html, done
+			return
+
+		case tagtok == html.StartTagToken:
+			tok := tokenizer.Token()
+
+			isAnchor := tok.Data == "a"
+
+			if isAnchor {
+				for _, attr := range tok.Attr {
+					if attr.Key == "href" {
+
+						if strings.Contains(attr.Val, url_prefix) {
+							fmt.Println(strings.Replace(attr.Val, url_prefix, "", 1))
+							fmt.Println("")
+							results += 1
+						}
+
+						break
+					}
+				}
+			}
+
+		case results == max_results:
+			return
+		}
+	}
 }
